60fbc6e56ddd3d207e87b3e35a7edb52ed79c28f
ludohenin <ludovic.henin@yahoo.com>
2015-12-02 22:00:56 +0100

    Refactor

--- lib/new.ts
+++ lib/new.ts
@@ -2,101 +2,33 @@ import {existsSync} from 'fs';
 import * as plugins from 'gulp-load-plugins';
 import * as gulp from 'gulp';
 import {join} from 'path';
-import {
-  PROJECT_CONFIG,
-  CWD,
-  REPLACE_FILE_PATTERN,
-  REPLACE_FOLDER_PATTERN,
-  TEMPLATES_DIR
-} from '../config';
-import {debug, log} from '../utils';
+import {REPLACE_FILE_PATTERN, REPLACE_FOLDER_PATTERN} from '../config';
+import {getSettings, log} from '../utils';
 require('colors');
-
 
 let $ = <any>plugins();
 
 export function CreateNewTemplate(tpl: string, path: string, options: any): void {
-  path = path.toLowerCase();
-  tpl  = tpl.toLowerCase();
-  let mappedName = mapName(tpl, path);
 
-  const SELECTOR       = mappedName.name;
-  const FOLDER_NAME    = SELECTOR.replace('-', '_');
-  const ASSET_PATH     = join(mappedName.path, FOLDER_NAME);
-  const DEST_PATH      = join(PROJECT_CONFIG.project_base, mappedName.path);
-  const DEST_PATH_ABS  = join(CWD, DEST_PATH);
-  const FILE_NAME      = `${FOLDER_NAME}_${tpl}`;
-  const CLASS_NAME     = classifyName(FILE_NAME);
-  const TEMPLATE_SRC   = join(TEMPLATES_DIR, tpl, '**/*');
+  let opts = getSettings(tpl, path);
 
-  debug('SELECTOR: ', SELECTOR);
-  debug('FOLDER_NAME: ', FOLDER_NAME);
-  debug('ASSET_PATH: ', ASSET_PATH);
-  debug('DEST_PATH: ', DEST_PATH);
-  debug('DEST_PATH_ABS: ', DEST_PATH_ABS);
-  debug('FILE_NAME: ', FILE_NAME);
-  debug('CLASS_NAME: ', CLASS_NAME);
-
-  if (existsSync(join(DEST_PATH_ABS, FOLDER_NAME))) {
-    log(`${DEST_PATH}/${FOLDER_NAME} already exist, add -f (--force) to force override it`.red);
+  if (existsSync(join(opts.DEST_PATH_ABS, opts.FOLDER_NAME))) {
+    log(`${opts.DEST_PATH}/${opts.FOLDER_NAME} already exist, add -f (--force) to force override it`.red);
     return;
   }
-  if (existsSync(join(DEST_PATH_ABS, FILE_NAME, '.ts'))) {
-    log(`${DEST_PATH}/${FILE_NAME}.ts already exist, add -f (--force) to force override it`.red);
+  if (existsSync(join(opts.DEST_PATH_ABS, opts.FILE_NAME, '.ts'))) {
+    log(`${opts.DEST_PATH}/${opts.FILE_NAME}.ts already exist, add -f (--force) to force override it`.red);
     return;
   }
 
-  gulp.src(TEMPLATE_SRC)
-    .pipe($.template({
-      ASSET_PATH,
-      CLASS_NAME,
-      DEST_PATH,
-      DEST_PATH_ABS,
-      FOLDER_NAME,
-      FILE_NAME,
-      SELECTOR
-    }))
-    .pipe(_renamePlugin(FOLDER_NAME, FILE_NAME))
-    .pipe(gulp.dest(DEST_PATH));
+  gulp.src(opts.TEMPLATE_SRC)
+    .pipe($.template(opts))
+    .pipe(_renamePlugin(opts.FOLDER_NAME, opts.FILE_NAME))
+    .pipe(gulp.dest(opts.DEST_PATH));
 }
 
 // ----------------
 // Utils.
-
-function classifyName(name: string): string {
-  return name.toLowerCase()
-    .replace(/[-_\.]+/g, ' ')
-    .trim()
-    .replace(/[^\w\s]/g, '')
-    .replace(/^./, $1 => $1.toUpperCase())
-    .replace(/ (.)/g, $1 => $1.toUpperCase())
-    .replace(/ /g, '' );
-}
-
-interface IMappedName {
-  name: string;
-  path: string;
-};
-
-function mapName(tpl: string, _name: string): IMappedName {
-  let frags = _name.split('/');
-  let name = frags.pop();
-  let path = getPath();
-
-  return {name, path};
-
-  function getPath(): string {
-    // TODO: Make configurable (do we enforce components structure ?).
-    //       enforcement would allow cmd: ng2-seed new cmp blog/pages/menu
-    let base = PROJECT_CONFIG['templates']['cmp']['folder_name'];
-    if (frags.length < 1) {
-      return base;
-    } else {
-      return join(base, frags.join(`/`));
-    }
-  }
-}
-
 function _renamePlugin(folderName, fileName) {
   let through = require('through2');
   return through.obj(function(file, enc, cb) {
--- utils.ts
+++ utils.ts
@@ -1,3 +1,5 @@
+import {join} from 'path';
+import {CWD, PROJECT_CONFIG, TEMPLATES_DIR} from './config';
 require('colors');
 
 
@@ -10,4 +12,73 @@ export function log(...args) {
 export function debug(...args) {
   if (!DEBUG) return;
   console.log('info: ', ...args);
+}
+
+export function getSettings(tpl, path) {
+  tpl  = tpl.toLowerCase();
+  path = path.toLowerCase();
+  let mappedName = mapName(tpl, path);
+
+  const SELECTOR       = mappedName.name;
+  const FOLDER_NAME    = SELECTOR.replace('-', '_');
+  const ASSET_PATH     = join(mappedName.path, FOLDER_NAME);
+  const DEST_PATH      = join(PROJECT_CONFIG.project_base, mappedName.path);
+  const DEST_PATH_ABS  = join(CWD, DEST_PATH);
+  const FILE_NAME      = `${FOLDER_NAME}_${tpl}`;
+  const CLASS_NAME     = classifyName(FILE_NAME);
+  const TEMPLATE_SRC   = join(TEMPLATES_DIR, tpl, '**/*');
+
+  debug('SELECTOR: ', SELECTOR);
+  debug('FOLDER_NAME: ', FOLDER_NAME);
+  debug('ASSET_PATH: ', ASSET_PATH);
+  debug('DEST_PATH: ', DEST_PATH);
+  debug('DEST_PATH_ABS: ', DEST_PATH_ABS);
+  debug('FILE_NAME: ', FILE_NAME);
+  debug('CLASS_NAME: ', CLASS_NAME);
+  debug('TEMPLATE_SRC: ', TEMPLATE_SRC);
+
+  return {
+    SELECTOR,
+    FOLDER_NAME,
+    ASSET_PATH,
+    DEST_PATH,
+    DEST_PATH_ABS,
+    FILE_NAME,
+    CLASS_NAME,
+    TEMPLATE_SRC
+  };
+}
+
+function classifyName(name: string): string {
+  return name.toLowerCase()
+    .replace(/[-_\.]+/g, ' ')
+    .trim()
+    .replace(/[^\w\s]/g, '')
+    .replace(/^./, $1 => $1.toUpperCase())
+    .replace(/ (.)/g, $1 => $1.toUpperCase())
+    .replace(/ /g, '' );
+}
+
+interface IMappedName {
+  name: string;
+  path: string;
+};
+
+function mapName(tpl: string, _name: string): IMappedName {
+  let frags = _name.split('/');
+  let name = frags.pop();
+  let path = getPath();
+
+  return {name, path};
+
+  function getPath(): string {
+    // TODO: Make configurable (do we enforce components structure ?).
+    //       enforcement would allow cmd: ng2-seed new cmp blog/pages/menu
+    let base = PROJECT_CONFIG['templates']['cmp']['folder_name'];
+    if (frags.length < 1) {
+      return base;
+    } else {
+      return join(base, frags.join(`/`));
+    }
+  }
 }
